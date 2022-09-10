35bd37a754df96529729d98147607cdaf261ea61
venkatamohan <venkatamohan80@gmail.com>
2022-09-10 17:35:57 +0530

    my first commit

---
+++ task1/src/App.js
@@ -0,0 +1,154 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div >
+      
+        <img
+          src="https://i.pinimg.com/originals/85/52/f8/8552f811e95b998d9505c43a9828c6d6.jpg"
+          alt="CSK Team Logo"
+          id="CSKLogo"
+        />
+        <h1 id="CSKTitle">Chennai Super Kings (CSK) Team</h1>
+        <h1 className="CSKPlayersNames">1.MS DHONI</h1>
+        <div className="playerdivs">
+          <img
+            src="https://www.deccanherald.com/sites/dh/files/gallery_images/2022/03/25/MS%20Dhoni%20Photos.jpg"
+            alt="Ms Dhoni pic"
+            className="playersImages"
+          />
+          <p className="playercontent">
+            Mahendra Singh Dhoni ( born 7 July 1981) is an Indian former
+            professional cricketer who was captain of the Indian national
+            cricket team in limited-overs formats from 2007 to 2017 and in Test
+            cricket from 2008 to 2014. He is a right-handed wicket-keeper
+            batsman. He led the team to three ICC trophies including the 2007
+            ICC World Twenty20, 2011 ICC Cricket World Cup and 2013 ICC
+            Champions Trophy. Under his captaincy, India won the ACC Asia Cup
+            two times, in 2010 and 2016. India also won ICC Test Championship
+            Mace two times in 2010, 2011 and ICC ODI Shield for one time in 2013
+            under his leadership. He is considered as one of the greatest
+            Captains and Wicket Keeper-Batsmen of all time. Throughout his 15
+            year long international career, Dhoni has won several awards and
+            accolades. Dhoni made his ODI debut on 23 December 2004, against
+            Bangladesh in Chittagong, and played his first Test a year later
+            against Sri Lanka. He played his first T20I also a year later
+            against South Africa. In 2007, he took over the ODI captaincy
+            from Rahul Dravid and he also selected as T20I captain of India in
+            this year. In 2008, he was selected as Test Captain. His
+            captaincy record in this format was mixed, successfully leading
+            India to a series win against New Zealand in 2008 and the
+            Border-Gavaskar Trophy (home series in 2010 and 2013) against
+            Australia while losing to Sri Lanka, Australia, England, and South
+            Africa by big margins in away conditions.
+          </p>
+        </div>
+        <h1 className="CSKPlayersNames">2.RAVINDRA JADEJA</h1>
+
+        <div className="playerdivs">
+          <img
+            src="https://c.ndtvimg.com/2019-05/3h42rn3s_ravindra-jadeja-csk-bcciipl_625x300_13_May_19.jpg"
+            alt="Ravindra jadeja pic"
+            className="playersImages"
+          />
+          <p className="playercontent">
+            Ravindrasinh Anirudhsinh Jadeja (born 6 December 1988), commonly
+            known as Ravindra Jadeja, is an Indian international cricketer. He
+            is an all-rounder, who bats left-handed and bowls left-arm orthodox
+            spin. He was the captain of the Chennai Super Kings in the Indian
+            Premier League. He represents Saurashtra in first-class cricket. He
+            is considered as one of the best fielders in the current decade.
+            Jadeja was vice-captain of the Indian U-19 cricket team that won the
+            World Cup in Malaysia in 2008, under the captaincy of former Indian
+            captain Virat Kohli. He made his ODI debut against Sri Lanka on 8
+            February 2009 and scored an unbeaten 60 off 77 balls in that match.
+            However, his Test debut came almost four years later, on 13 December
+            2012, against England at Nagpur. Jadeja was bought for $2 million by
+            the Chennai Super Kings at the 2012 IPL Players Auction. He was
+            bought by the Gujarat Lions in the 2016 IPL Players Auction for ₹9.5
+            crores after the Chennai Super Kings were banned from the IPL for
+            two seasons. On 22 January 2017, Jadeja became the first Indian
+            left-arm spinner to take 150 One Day International wickets, when he
+            dismissed Sam Billings at Eden Gardens, Kolkata. In March 2017, he
+            became the top ranked bowler in the world leaving behind
+            Ravichandran Ashwin who held that position for a long time. He was
+            announced as captain of the Chennai Super Kings IPL franchise, for
+            the 2022 IPL season, succeeding MS Dhoni.He however stepped down in
+            the middle of the season.
+          </p>
+        </div>
+        <h1 className="CSKPlayersNames">3.AMBATI RAYUDU</h1>
+
+        <div className="playerdivs">
+          <img
+            src="https://www.wicketnrun.com/wp-content/uploads/2021/03/Ambati-Rayudu-IPL-Controversies-and-Career.jpg"
+            alt="Ambati Rayudu pic"
+            className="playersImages"
+          />
+          <p className="playercontent">
+            Ambati Thirupathi Rayudu (born 23 September 1985) is an Indian
+            professional cricketer and current captain of the Andhra team in the
+            Syed Mushtaq Ali Trophy championship. He plays as a right-handed
+            middle-order batsman, who occasionally keeps wicket and bowls
+            right-arm off breaks. He plays for Hyderabad in domestic cricket,
+            and Chennai Super Kings in the Indian Premier League (IPL). Rayudu
+            began his first-class career with Hyderabad in 2002 at the age of 16
+            and, by the following year, was playing for India A. He captained
+            the India Under-19 team at the 2004 ICC Under-19 Cricket World Cup
+            and was expected to break into the senior team. Disputes with
+            players and state association, followed by signing up with the
+            "rebel" Indian Cricket League, led to him being ignored for national
+            team selection. He returned to domestic cricket in 2009 by accepting
+            BCCI's amnesty offer and terminating his ICL contract. After strong
+            performances for Baroda in domestic cricket and Mumbai Indians in
+            the IPL, he was named in the Indian squad for the first time in
+            2012. He eventually made his international debut in July 2013, in an
+            ODI against Zimbabwe. On 2 July 2019, he announced his retirement
+            from all forms of cricket. However, on 29 August 2019, Rayudu said
+            that he was willing to play all formats of cricket again.
+          </p>
+        </div>
+        <h1 className="CSKPlayersNames">4.BRAVO</h1>
+
+        <div className="playerdivs">
+          <img
+            src="https://c.ndtvimg.com/2022-03/bgjeul7_dwayne-bravo_625x300_31_March_22.jpg"
+            alt="Bravo pic"
+            className="playersImages"
+          />
+          <p className="playercontent">
+            Dwayne John Bravo (born 7 October 1983) is a Trinidadian cricketer
+            and a former captain of the West Indies cricket team. A genuine
+            right arm fast bowling all-rounder, well known for his aggressive
+            batting in the lower order, and for the variety of lengths in last
+            overs. During his prime, he was regarded as one of the best death
+            bowlers in T20 Cricket. He also performs as a singer. Since 2004,
+            Bravo has played 40 Test matches, 164 One Day Internationals and 91
+            Twenty20 Internationals for the West Indies. He was a key member of
+            the West Indies team that won the 2004 ICC Champions Trophy, the
+            2012 ICC World Twenty20 and the 2016 ICC World Twenty20. In domestic
+            cricket, Bravo has played for his native Trinidad and Tobago since
+            2002. He has also played for the Chennai Super Kings, Gujarat Lions
+            and Mumbai Indians in the Indian Premier League, the Lahore
+            Qalanders and Quetta Gladiators in the Pakistan Super League, the
+            Melbourne Stars and Melbourne Renegades in the Big Bash League, the
+            Chittagong Kings in the Bangladesh Premier League, and Kent and
+            Essex in English county cricket. In 2013, he was named as a
+            franchise player at the launch of the Caribbean Premier League.
+            After initially announcing his retirement from international cricket
+            in October 2018, Bravo came out of retirement in December 2019 in
+            preparation for the 2020 T20 World Cup. On 10th October 2021, while
+            playing with Chennai Super Kings, Bravo completed 550 wickets in his
+            T20 career in the Qualifier 1 match of Indian Premier League against
+            Delhi Capitals.
+          </p>
+          
+        </div>
+        <a href="app.js">Home page</a>
+      
+    </div>
+  );
+}
+
+export default App;
